42ff82b77256a57d81bba7eddb91ded9c28770ce
AryanLagad <156797106+AryanLagad@users.noreply.github.com>
2026-01-15 23:15:32 +0530

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,6 @@
 def call(){
-  timeout(time: 10, unit: "MINUTES"){
+  sleep(time: 5, unit: "SECONDS")  // Give webhook time to arrive
+  timeout(time: 5, unit: "MINUTES"){
       waitForQualityGate abortPipeline: false
   }
 }
